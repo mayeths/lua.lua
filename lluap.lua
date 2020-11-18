4eef54021a53cfc6d71d0103219f45ad77f7c41a
mayeths <wow@mayeths.com>
2020-11-18 18:16:09 +0800

    Add LLUAP for printing Lua bytecode proto info

---
+++ lluap.lua
@@ -0,0 +1,146 @@
+require("runtime/binaryChunk/binaryChunk")
+require("runtime/vm/Instruction")
+require("runtime/util/util")
+
+
+LLUAP = {}
+
+
+function LLUAP:main()
+    if #arg < 1 then
+        Util:panic("[LLUAP ERROR] Running LLUAP require a bytecode file")
+    end
+    local fd = io.open(arg[1], "rb")
+    local data = fd:read("*all")
+    fd:close()
+    local proto = BinaryChunk:Undump(data)
+    LLUAP:displayProtoInfo(proto)
+end
+
+
+function LLUAP:displayProtoInfo(proto)
+    LLUAP:printProtoHeader(proto)
+    LLUAP:printProtoCode(proto)
+    LLUAP:printProtoDetail(proto)
+    LLUAP:printProtoFooter()
+end
+
+
+function LLUAP:printProtoHeader(proto)
+    local protoType = "main"
+    local varargFlag = ""
+    if proto.LineDefined > 0 then
+        protoType = "function"
+    end
+    if proto.IsVararg > 0 then
+        varargFlag = "+vararg"
+    end
+    Util:println("--- PROTO %s <%s:%d-%d> (%d instructions) ---",
+        protoType, proto.Source, proto.LineDefined,
+        proto.LastLineDefined, #proto.Code
+    )
+    Util:printf("%d%s params, %d slots, %d upvalues, ",
+        proto.NumParams, varargFlag,
+        proto.MaxStackSize, #proto.Upvalues
+    )
+    Util:println("%d constants, %d locals, %d functions",
+        #proto.Constants, #proto.LocVars, #proto.Protos
+    )
+end
+
+
+function LLUAP:printProtoCode(proto)
+    Util:println("body (%d):", #proto.Code)
+    Util:println("\tindex\tline\tinstruction\topname\t\toperand")
+    for i, code in ipairs(proto.Code) do
+        local line = "-"
+        if #proto.LineInfo > 0 then
+            line = string.format("%d", proto.LineInfo[i])
+        end
+        local inst = Instruction:new({value = code})
+        Util:printf("\t%d\t[%s]\t0x%08X\t%s\t", i, line, code, inst:OpName())
+        LLUAP:printOperands(inst)
+        Util:println("")
+    end
+end
+
+
+function LLUAP:printOperands(inst)
+    local mode = inst:OpMode()
+    if mode == OPMODE.IABC then
+        local a, b, c = inst:ABC()
+        Util:printf("%d", a)
+        if inst:BMode() ~= OPARGMASK.OpArgN then
+            if b > 0xFF then
+                Util:printf(" %d", -1-(b&0xFF))
+            else
+                Util:printf(" %d", b)
+            end
+        end
+        if inst:CMode() ~= OPARGMASK.OpArgN then
+            if c > 0xFF then
+                Util:printf(" %d", -1-(c&0xFF))
+            else
+                Util:printf(" %d", c)
+            end
+        end
+    elseif mode == OPMODE.IABx then
+        local a, bx = inst:ABx()
+        Util:printf("%d", a)
+        if inst:BMode() == OPARGMASK.OpArgK then
+            Util:printf(" %d", -1-bx)
+        elseif inst:BMode() == OPARGMASK.OpArgU then
+            Util:printf(" %d", bx)
+        end
+    elseif mode == OPMODE.IAsBx then
+        local a, sbx = inst:AsBx()
+        Util:printf("%d %d", a, sbx)
+    elseif mode == OPMODE.IAx then
+        local ax = inst:Ax()
+        Util:printf("%d", -1-ax)
+    end
+end
+
+
+function LLUAP:printProtoDetail(proto)
+    Util:println("constants (%d):", #proto.Constants)
+    for i, const in ipairs(proto.Constants) do
+        Util:println("\t%d\t%s", i, LLUAP:constantToString(const))
+    end
+
+    Util:println("locals (%d):", #proto.LocVars)
+    for i, locvar in ipairs(proto.LocVars) do
+        Util:println("\t%d\t%s\t%d\t%d",
+            i, locvar.VarName, locvar.StartPC, locvar.EndPC
+        )
+    end
+
+    Util:println("upvalues (%d):", #proto.Upvalues)
+    for i, upval in ipairs(proto.Upvalues) do
+        local upvalName = "-"
+        if #proto.UpvalueNames > 0 then
+            upvalName = proto.UpvalueNames[i]
+        end
+        Util:println("\t%d\t%s\t%d\t%d",
+            i, upvalName, upval.Instack, upval.Idx
+        )
+    end
+end
+
+
+function LLUAP:printProtoFooter()
+    Util:println("------")
+end
+
+
+
+function LLUAP:constantToString(const)
+    if type(const) == "string" then
+        return '"'..const..'"'
+    else
+        return tostring(const)
+    end
+end
+
+
+LLUAP:main()
