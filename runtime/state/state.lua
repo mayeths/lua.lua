048d85c470321fbeba605109bc60bb709ffb9027
mayeths <wow@mayeths.com>
2020-11-18 19:37:31 +0800

    Uppercase API* of runtime/state

--- runtime/state/state.lua
+++ runtime/state/state.lua
@@ -1,6 +1,6 @@
 require("runtime/state/luaState")
 -- Mount LuaState operating functions from following files
 -- These api are implements of http://www.lua.org/manual/5.3/manual.html#4.8
-require("runtime/state/apiOperateStack")
-require("runtime/state/apiPush")
-require("runtime/state/apiValueType")
+require("runtime/state/APIOperateStack")
+require("runtime/state/APIPush")
+require("runtime/state/APIValueType")
