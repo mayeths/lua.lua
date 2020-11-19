1312b7535b72a017db677d104c8425453eac7a77
mayeths <wow@mayeths.com>
2020-11-19 16:24:25 +0800

    Fix require APIPushValue

--- runtime/state/state.lua
+++ runtime/state/state.lua
@@ -2,5 +2,5 @@ require("runtime/state/luastate")
 -- Mount LuaState operating functions from following files
 -- These api are implements of http://www.lua.org/manual/5.3/manual.html#4.8
 require("runtime/state/APIOperateStack")
-require("runtime/state/APIPush")
+require("runtime/state/APIPushValue")
 require("runtime/state/APIValueType")
