d325e2d3864c14be50adbee9d36ad048b344104d
mayeths <wow@mayeths.com>
2021-01-23 22:01:38 +0800

    Add color print

---
+++ common/color.lua
@@ -0,0 +1,17 @@
+Color = {
+    RED = "\27[31m",
+    GREEN = "\27[32m",
+    ORANGE = "\27[33m",
+    BLUE = "\27[34m",
+    PURPLE = "\27[35m",
+    CYAN = "\27[36m",
+    NC = "\27[0m",
+}
+
+function Color:red(str)
+    return self.RED..str..self.NC
+end
+
+function Color:blue(str)
+    return self.BLUE..str..self.NC
+end
