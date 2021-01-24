b9697f367c9bc7eaba8d279cfc1d45fbb83f669c
mayeths <wow@mayeths.com>
2021-01-24 14:50:22 +0800

    Add command info for lluar

--- runtime/lluar.lua
+++ runtime/lluar.lua
@@ -1,11 +1,16 @@
+require("common/util")
 require("common/arg")
-
+LLUAR = {
+    NAME = "Lua.lua runtime",
+    VERSION = "0.1.0",
+    INFO = "Copyright (C) 2021 Mayeths",
+}
 LLUAR_ARG = ARG:new("lluar", "The runtime of lua.lua", "-",
     {
         param_name = "binarychunk",
         unmatched_name = "args"
     }, {
-        HELP = ARG:boolopt("h", "Print usage"),
+        HELP = ARG:boolopt("h", "Print this help"),
         VERSION = ARG:boolopt("v", "Print current version"),
         OUTPUT = ARG:stropt("o", "Output to specific file", "a.out"),
         DEBUG_LEVEL = ARG:enumopt(
@@ -15,5 +20,22 @@ LLUAR_ARG = ARG:new("lluar", "The runtime of lua.lua", "-",
     }
 )
 
-LLUAR_ARG:parse(arg)
-print(LLUAR_ARG:tostring())
+
+function LLUAR:main()
+    LLUAR_ARG:parse(arg)
+    if LLUAR_ARG.HELP == true then
+        Util:println(LLUAR_ARG:tostring())
+        return
+    elseif LLUAR_ARG.VERSION == true then
+        local arr = { LLUAR.NAME, LLUAR.VERSION, LLUAR.INFO }
+        local msg = table.concat(arr, " ")
+        Util:println(msg)
+        return
+    end
+end
+
+
+
+
+LLUAR:main()
+
