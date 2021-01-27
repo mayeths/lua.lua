81657b50d000ca07cbe49944db7cdd16857f8310
mayeths <wow@mayeths.com>
2021-01-27 19:47:55 +0800

    Move APIOperateValue to luastate

--- runtime/state/state.lua
+++ runtime/state/state.lua
@@ -1,6 +1,5 @@
 -- Mount LuaState functions from following files
 -- These api implement http://www.lua.org/manual/5.3/manual.html#4.8
-require("runtime/state/APIOperateValue")
 require("runtime/state/APIPushValue")
 require("runtime/state/APIValueType")
 require("runtime/state/APIVM")
