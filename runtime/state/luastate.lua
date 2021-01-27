ea9ced4fc275004b66eff51fe23c2a92fe965a1d
mayeths <wow@mayeths.com>
2021-01-27 19:51:15 +0800

    Move APIVM to luastate

--- runtime/state/luastate.lua
+++ runtime/state/luastate.lua
@@ -1,5 +1,6 @@
 local LuaStack = require("runtime/state/luastack")
 local LuaType = require("runtime/state/luatype")
+local LuaClosure = require("runtime/state/luaclosure")
 local LuaOperation = require("runtime/state/luaoperation")
 local Arith = require("runtime/operation/arithmetic")
 local Compare = require("runtime/operation/compare")
@@ -445,6 +446,60 @@ function LuaState:ToStringX(idx)
         return "", false
     end
 end
+
+
+function LuaState:PC()
+    return self.stack.pc
+end
+
+
+function LuaState:AddPC(n)
+    self.stack.pc = self.stack.pc + n
+end
+
+
+function LuaState:Fetch()
+    self.stack.pc = self.stack.pc + 1
+    local i = self.stack.closure.proto.Code[self.stack.pc]
+    return i
+end
+
+
+function LuaState:GetConst(idx)
+    local c = self.stack.closure.proto.Constants[idx + 1]
+    self.stack:push(c)
+end
+
+
+function LuaState:GetRK(rk)
+    if rk > 0xFF then
+        self:GetConst(rk & 0xFF)
+    else
+        self:PushValue(rk + 1)
+    end
+end
+
+
+function LuaState:RegisterCount()
+    return self.stack.closure.proto.MaxStackSize
+end
+
+
+function LuaState:LoadVararg(n)
+    if n < 0 then
+        n = #self.stack.varargs
+    end
+    self.stack:ensure(n)
+    self.stack:pushN(self.stack.varargs, n)
+end
+
+
+function LuaState:LoadProto(idx)
+    local proto = self.stack.closure.proto.Protos[idx]
+    local closure = LuaClosure:new(proto)
+    self.stack:push(closure)
+end
+
 
 
 
--- runtime/state/state.lua
+++ runtime/state/state.lua
@@ -1,6 +1,5 @@
 -- Mount LuaState functions from following files
 -- These api implement http://www.lua.org/manual/5.3/manual.html#4.8
-require("runtime/state/APIVM")
 require("runtime/state/APITable")
 require("runtime/state/APICall")
 
