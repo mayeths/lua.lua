a9c294e5481a807b521298c6ece0eb2e91ae63fe
mayeths <wow@mayeths.com>
2021-01-27 19:53:55 +0800

    Move APICall to luastate

--- runtime/state/luastate.lua
+++ runtime/state/luastate.lua
@@ -2,10 +2,13 @@ local LuaStack = require("runtime/state/luastack")
 local LuaType = require("runtime/state/luatype")
 local LuaClosure = require("runtime/state/luaclosure")
 local LuaOperation = require("runtime/state/luaoperation")
+local BinaryChunk = require("runtime/binarychunk/binarychunk")
 local Arith = require("runtime/operation/arithmetic")
 local Compare = require("runtime/operation/compare")
 local Convert = require("runtime/operation/convert")
 local Logical = require("runtime/operation/logical")
+local Instruction = require("runtime/vm/instruction")
+local OPCODE = require("runtime/vm/opcode")
 local Util = require("common/util")
 
 local LuaState = {
@@ -552,6 +555,61 @@ function LuaState:SetI(idx, i)
     local t = self.stack:get(idx)
     local v = self.stack:pop()
     t.table[i] = v
+end
+
+
+function LuaState:Load(chunk, name, mode)
+    local proto = BinaryChunk:Undump(chunk)
+    local closure = LuaClosure:new(proto)
+    self.stack:push(closure)
+end
+
+
+function LuaState:Call(nRealParams, nRealResults)
+    local closure = self.stack:get(-(nRealParams + 1))
+    if not LuaClosure:isClosure(closure) then
+        Util:panic("[LuaState:Call ERROR] not a function")
+    end
+    local proto = closure.proto
+    Util:printf("calling %s<%d,%d>\n", proto.Source,
+        proto.LineDefined, proto.LastLineDefined)
+    local nreg = proto.MaxStackSize
+    local nDefinedparams = proto.NumParams
+    local isVararg = proto.IsVararg == 1
+    local newStack = LuaStack:new(nreg + 20)
+    newStack.closure = closure
+    local realParams = self.stack:popN(nRealParams)
+    local func = self.stack:pop()
+    newStack:pushN(realParams, nDefinedparams)
+    newStack:settop(nreg)
+    if nRealParams >= nDefinedparams and isVararg then
+        local varargParams = {}
+        for i = nDefinedparams + 1, #realParams do
+            varargParams[#varargParams + 1] = realParams[i]
+        end
+        newStack.varargs = varargParams
+    end
+
+    self:pushLuaStack(newStack)
+    self:runClosure()
+    self:popLuaStack()
+
+    if nRealResults ~= 0 then
+        local result = newStack:popN(newStack:gettop() - nreg)
+        self.stack:ensure(#result)
+        self.stack:pushN(result, nRealResults)
+    end
+end
+
+
+function LuaState:runClosure()
+    while true do
+        local inst = Instruction:new(self:Fetch())
+        inst:Execute(self)
+        if inst:Opcode() + 1 == OPCODE.OP_RETURN then
+            break
+        end
+    end
 end
 
 
--- runtime/state/state.lua
+++ runtime/state/state.lua
@@ -1,5 +1,4 @@
 -- Mount LuaState functions from following files
 -- These api implement http://www.lua.org/manual/5.3/manual.html#4.8
-require("runtime/state/APICall")
 
 return require("runtime/state/luastate")
