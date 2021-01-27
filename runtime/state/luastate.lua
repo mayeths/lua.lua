97084da5010cd732fd40937747cea327a67b1763
mayeths <wow@mayeths.com>
2021-01-27 19:49:04 +0800

    Move APIPushValue to luastate

--- runtime/state/luastate.lua
+++ runtime/state/luastate.lua
@@ -245,6 +245,37 @@ function LuaState:Concat(n)
 end
 
 
+function LuaState:PushNil()
+    self.stack:push(nil)
+end
+
+
+function LuaState:PushBoolean(b)
+    Util:assert(type(b), "boolean",
+        "Pushing a non-boolean value by PushBoolean method"
+    )
+    self.stack:push(b)
+end
+
+
+function LuaState:PushInteger(n)
+    Util:assert(math.type(n), "integer",
+        "Pushing a non-integer value by PushInteger method"
+    )
+    self.stack:push(n)
+end
+
+
+function LuaState:PushNumber(n)
+    self.stack:push(n)
+end
+
+
+function LuaState:PushString(s)
+    self.stack:push(s)
+end
+
+
 
 
 
